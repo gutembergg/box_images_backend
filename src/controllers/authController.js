d6694f01287e6c7873bbb2e98f78bb76df13066a
Gutembergue Mascarenhas <gmascarenhas3001@gmail.com>
2020-07-22 17:20:29 +0200

    add jwtoken, create authentication

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,20 +1,50 @@
 const express = require('express')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const User = require('../models/User')
+const authConfig = require('../config/authConfig.json')
 
 const router = express.Router()
 
 router.post('/register', async (req, res) => {
   const { email } = req.body
   try {
-    if (User.findOne({ email })) {
+    if (await User.findOne({ email })) {
       return res.status(400).send({ error: 'User already exists' })
     }
     const user = await User.create(req.body)
 
+    user.password = undefined
+
     return res.send({ user })
   } catch (err) {
     return res.status(400).send({ Error: 'Error register faild' })
+  }
+})
+
+router.post('/authenticate', async (req, res) => {
+  try {
+    const { email, password } = req.body
+
+    const user = await User.findOne({ email }).select('+password')
+
+    if (!user) {
+      return res.status(400).send({ Error: 'User not found' })
+    }
+    if (!(await bcrypt.compare(password, user.password))) {
+      return res.status(401).send({ Error: 'Invalid password' })
+    }
+
+    user.password = undefined
+
+    const token = jwt.sign({ id: user.id }, authConfig.secret, {
+      expiresIn: 86400
+    })
+
+    return res.send({ user, token })
+  } catch (err) {
+    return res.status(400).send({ Error: 'Error in authenticate' })
   }
 })
 
